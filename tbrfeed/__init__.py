8fda71eca6ac50cbfdb0fe14117ee36de39f7455
azyobuzin <azyobuzin@users.sourceforge.jp>
2014-08-24 01:38:53 +0900

    CSRF protection

--- tbrfeed/__init__.py
+++ tbrfeed/__init__.py
@@ -25,12 +25,23 @@ def tumblr_client(oauth_token=None, oauth_token_secret=None):
         os.environ.get("TBRFEED_CONSUMER_SECRET"),
         oauth_token, oauth_token_secret)
 
+def create_hash():
+    return hashlib.sha1((username + str(time.time()) + str(random.randint(0, 99999))).encode("utf-8")).hexdigest()
+
+def csrf_protect():
+    token = flask.session.pop("_csrf_token", None)
+    if not token or token != flask.request.args.get("csrf_token"):
+        abort(400)
+
 @app.route("/")
 def index():
-    return flask.render_template("loggedin.html" if "user_id" in flask.session else "unlogin.html")
+    csrf_token = create_hash()
+    flask.session["_csrf_token"] = create_hash()
+    return flask.render_template("loggedin.html" if "user_id" in flask.session else "unlogin.html", csrf_token=csrf_token)
 
 @app.route("/authorize")
 def authorize():
+    csrf_protect()
     client = tumblr_client()
     client.request_token("http://%s/callback" % flask.request.headers.get("Host"))
     flask.session["request_secret"] = client.oauth_token_secret
@@ -52,7 +63,7 @@ def callback():
 
     info = client.user_info()
     username = info["name"]
-    id = hashlib.sha1((username + str(time.time()) + str(random.randint(0, 99999))).encode("utf-8")).hexdigest()
+    id = create_hash()
     
     with database.Connection() as cursor:
         cursor.execute("SELECT update_user(%s, %s, %s, %s)", (id, username, client.oauth_token, client.oauth_token_secret))
@@ -65,12 +76,14 @@ def callback():
 
 @app.route("/logout")
 def logout():
+    csrf_protect()
     del flask.session["user_id"]
     del flask.session["user_name"]
     return flask.redirect(flask.url_for("index"))
 
 @app.route("/suspend")
 def suspend():
+    csrf_protect()
     with database.Connection() as cursor:
         cursor.execute("DELETE FROM users WHERE id = %s", (flask.session["user_id"],))
     return logout()
